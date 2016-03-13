1d88996060334bed2dc16191c5fd5b25b0736ae8
Andrew Hill <mdude2314@gmail.com>
2016-03-13 10:42:19 -0400

    loaded buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -423,7 +423,7 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
+    grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve:' + target]);
   });

8a3da1a5daff656e53cecb0a45916097a6b554e7
Andrew Hill <mdude2314@gmail.com>
2016-03-15 18:22:30 -0400

    Revert "merging into develop"

---
+++ app/scripts/angularfire/config.js
@@ -0,0 +1,2 @@
+angular.module('firebase.config', [])
+  .constant('FBURL', 'https://burning-heat-1866.firebaseio.com');
---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularfireApp
+ * @description
+ * # angularfireApp
+ *
+ * Main module of the application.
+ */
+angular.module('angularfireApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngTouch',
+    'firebase',
+    'firebase.ref'
+  ]);
